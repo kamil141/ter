d4acfb5a833233bfae74c8c27a070129224fff8a
Kamil Hajiyev <Kamil.Hajiyev@rescue.org>
2022-08-16 16:09:22 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # VERSION IS NOT NEEDED HERE
+}
+
+
+resource "aws_iam_user" "my_iam_users" {
+  count = 3
+  name  = "my_iam_user_${count.index}"
+
+
+}
